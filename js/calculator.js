745362591602753248310163596a3950b47bd0fd
Emilio Benavides <emilio.benavides68@gmail.com>
2022-09-06 21:52:11 -0500

    added appendnumber function

--- js/calculator.js
+++ js/calculator.js
@@ -5,6 +5,7 @@ class Calculator {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
         this.clear()
+    }
 }
 
 const numberButtons = document.querySelectorAll('[data-number]');
@@ -14,4 +15,40 @@ const deleteButton = document.querySelectorAll('[data-delete]');
 const allClearButton = document.querySelectorAll('[data-all-clear]');
 const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]');
 const currentOperandTextElement = document.querySelectorAll('[data-current-operand]');
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+
+function clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+}
+
+function delete() {
+
+}
+
+function appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.'))
+        return this.currentOperand = this.currentOperand.toString() + number.toString()
+}
+
+function chooseOperation(operation) {
+
+}
+
+function compute() {
+
+}
+
+function updateDisplay() {
+this.currentOperandTextElement.innerText = this.currentOperand
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
 
