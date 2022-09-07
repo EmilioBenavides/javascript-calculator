640146bf00e3e30b823d04c6b0b38c3f72719efd
Emilio Benavides <emilio.benavides68@gmail.com>
2022-09-06 21:14:54 -0500

    added a calculator class

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,12 @@
 "use strict";
 
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+}
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelectorAll('[data-equals]');
@@ -7,3 +14,4 @@ const deleteButton = document.querySelectorAll('[data-delete]');
 const allClearButton = document.querySelectorAll('[data-all-clear]');
 const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]');
 const currentOperandTextElement = document.querySelectorAll('[data-current-operand]');
+
