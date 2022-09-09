771c5d88d7e18c9b4da22180aeecdfd4a068d4d1
Emilio Benavides <emilio.benavides68@gmail.com>
2022-09-08 22:02:30 -0500

    added compute function

--- js/calculator.js
+++ js/calculator.js
@@ -71,6 +71,31 @@ equalsButton.addEventListener('click', button => {
     calculator.updateDisplay()
 })
 
+function compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current)) return
+    switch (this.operation) {
+        case '+':
+            computation = prev + current
+            break
+        case '-':
+            computation = prev - current
+            break
+        case '*':
+            computation = prev * current
+            break
+        case '&#247':
+            computation = prev / current
+            break
+        default:
+            return
+    }
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ''
+}
 
 
 
